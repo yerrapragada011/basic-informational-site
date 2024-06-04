aeca0b1145fe7b77fd162777dce404e2bf97db38
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-06-04 15:16:37 -0400

    basic info site

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true)
+    var filename = q.pathname === '/' ? './index.html' : '.' + q.pathname
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        fs.readFile('./404.html', function (err, data) {
+          if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' })
+            return res.end('404 Not Found')
+          }
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.write(data)
+          return res.end()
+        })
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(data)
+        return res.end()
+      }
+    })
+  })
+  .listen(8080)
